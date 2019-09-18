ed3965f7bb429e984c271006f4a296e730dc1fdc
benclarkadvam <ben.clark@advam.com>
2019-09-18 10:39:30 +0930

    move index to root

---
+++ index.js
@@ -0,0 +1,9 @@
+import { app as a } from './src/fns/app/app'
+import { dispatch as d } from './src/fns/dispatch/dispatch'
+import { qs as q} from './src/fns/qs/qs'
+import { replace as r } from './src/fns/replace/replace'
+
+export const app = a
+export const dispatch = d
+export const qs = q
+export const replace = r
