0d13a558a5b81869db2bcb904b8b01deb00a6c02
Benjamin Clark <benjaminclark@Benjamins-Mac-mini.local>
2019-09-24 05:46:25 +0930

    route changes

--- src/fns/app/app.js
+++ src/fns/app/app.js
@@ -1,11 +1,29 @@
-import { replace } from "../replace/replace"
+import { replace } from '../replace/replace'
+import { returnRouteObject } from '../return-route-object/return-route-object'
 
 export let state, reducer, tree
 
+const updateStateAndRender = (action) => {
+  const oldState = JSON.parse(JSON.stringify(state))
+  const route = returnRouteObject()
+  updateState(
+    Object.assign({}, reducer(state, action), { route })
+  )
+  tree(oldState, state, action)
+}
+
 export const app = ($state, $component, $mount, $reducer, $tree) => {
-  state = $state
+  state = { ...$state, route: returnRouteObject() }
   reducer = $reducer
   tree = $tree
+
+  window.onpopstate = () => {
+    updateStateAndRender({ type: 'ROUTE_CHANGE', route: returnRouteObject()})
+  }
+  window.onhashchange = () => {
+    updateStateAndRender({ type: 'ROUTE_CHANGE', route: returnRouteObject()})
+  }
+
   replace($mount, $component({ state }))
 }
 
